bbd1f03d86349637b2b1fbdf238558bc7987161a
Vladislav Grechannik <52157081+VlaDexa@users.noreply.github.com>
2024-07-16 18:05:40 +0200

    which-key v3 update (#1022)
    * which-key v3 update
    
    * remove unneeded brackets from which-key registration

--- init.lua
+++ init.lua
@@ -358,28 +358,15 @@ require('lazy').setup({
       require('which-key').setup()
 
       -- Document existing key chains
-      require('which-key').register {
-        {
-          { '<leader>c', group = '[C]ode' },
-          { '<leader>c_', hidden = true },
-          { '<leader>d', group = '[D]ocument' },
-          { '<leader>d_', hidden = true },
-          { '<leader>h', group = 'Git [H]unk' },
-          { '<leader>h_', hidden = true },
-          { '<leader>r', group = '[R]ename' },
-          { '<leader>r_', hidden = true },
-          { '<leader>s', group = '[S]earch' },
-          { '<leader>s_', hidden = true },
-          { '<leader>t', group = '[T]oggle' },
-          { '<leader>t_', hidden = true },
-          { '<leader>w', group = '[W]orkspace' },
-          { '<leader>w_', hidden = true },
-        },
+      require('which-key').add {
+        { '<leader>c', group = '[C]ode' },
+        { '<leader>d', group = '[D]ocument' },
+        { '<leader>r', group = '[R]ename' },
+        { '<leader>s', group = '[S]earch' },
+        { '<leader>w', group = '[W]orkspace' },
+        { '<leader>t', group = '[T]oggle' },
+        { '<leader>h', group = 'Git [H]unk', mode = { 'n', 'v' } },
       }
-      -- visual mode
-      require('which-key').register({
-        ['<leader>h'] = { 'Git [H]unk' },
-      }, { mode = 'v' })
     end,
   },
 
